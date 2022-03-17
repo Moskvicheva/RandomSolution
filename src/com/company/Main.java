3640a94514a274afb7da6d050cb7ab29841b6321
Moskvicheva <paulines.letter@yandex.ru>
2022-03-17 03:02:36 +0300

    My solution for task from Hyperskill (gaussian random numbers)

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long k = scanner.nextLong();
+        int n = scanner.nextInt();
+        double m = scanner.nextDouble();
+        Random random;
+        boolean ok = true;
+        int counter = 0;
+
+        while (ok) {
+            random = new Random(k);
+            for (int i = 0; i < n; i++) {
+                if (random.nextGaussian() > m) {
+                    k += 1;
+                    break;
+                }
+                counter += 1;
+            }
+            if (counter == n) {
+                System.out.println(k);
+                ok = false;
+            } else {
+                counter = 0;
+            }
+        }
+
+    }
+}
+
